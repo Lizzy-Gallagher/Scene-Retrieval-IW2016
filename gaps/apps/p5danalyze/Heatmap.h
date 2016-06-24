5825f4dd73d8dadf3b8d2f1d2d3c7bb2beb7df47
LizzyBradley <erb2@princeton.edu>
2016-06-24 13:32:56 -0400

    Continued refactoring, beginning preposisition work.

--- gaps/apps/p5danalyze/Heatmap.h
+++ gaps/apps/p5danalyze/Heatmap.h
@@ -5,6 +5,8 @@
 
 #include "R3Graphics/R3Graphics.h"
 #include "StatsAux.h"
+#include "R2Aux.h"
+#include "P5DAux.h"
 
 #include <string>
 #include <map>
@@ -17,5 +19,8 @@ int WriteHeatmaps(HeatmapMap* heatmaps, FrequencyStats freq_stats,
 void UpdateHeatmaps(R3Scene* scene, std::vector<R3SceneNode*> objects, 
         HeatmapMap* heatmaps, int resolution, std::map<std::string, int>* cat_count, 
         Id2CatMap* id2cat);
+void CalcHeatmaps( R3SceneNode* pri_obj, R3SceneNode* ref_obj, std::string ref_cat, 
+        std::string pri_cat, Id2CatMap* id2cat, HeatmapMap* heatmaps, 
+        DrawingValues values, double threshold, int pixels_to_meters);
 
 #endif
