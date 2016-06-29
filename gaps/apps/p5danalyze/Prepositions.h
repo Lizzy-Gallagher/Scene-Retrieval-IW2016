6b047ff3eeb369c977e40f9c113671fc5270b451
LizzyBradley <erb2@princeton.edu>
2016-06-29 15:26:39 -0400

    Working prepositions for single scene

--- gaps/apps/p5danalyze/Prepositions.h
+++ gaps/apps/p5danalyze/Prepositions.h
@@ -1,8 +1,8 @@
 // This is the header file for the Preposition Analysis
 
 
-#ifndef PREP
-#define PREP
+#ifndef PREPS
+#define PREPS
 
 #include "R3Graphics/R3Graphics.h"
 #include "IOAux.h"
@@ -22,7 +22,71 @@ struct PrepositionStats {
 
 using PrepMap = std::map<std::string, std::map<std::string, PrepositionStats>>;
 
+enum Prep {
+    PREP_FRONTSIDE   = 0,
+    PREP_BACKSIDE    = 1,
+    PREP_ABOVE       = 2,
+    PREP_BELOW       = 3,
+    PREP_RIGHTSIDE   = 4,
+    PREP_LEFTSIDE    = 5,
+    NUM_PREPOSITIONS = 6,
+};
+
+struct PrepRegion {
+    const char* name;
+    int prep;
+    R3Box region;
+};
+
+/*class PrepRegion {
+    public:
+        // Constructor Functions
+        PrepRegion(const char* name, int prep, R3Box region);
+        PrepRegion(const PrepRegion& prep_region);
+
+        // Access Functions
+        const char *Name(void);
+        int Preposition(void);
+        R3Box Region(void);
+
+    private:
+        const char* name;
+        int prep;
+        R3Box region;
+};
+
+inline PrepRegion::PrepRegion(const char* name, int prep, R3Box region) : name(name), prep(prep), region(region)
+{
+    fprintf(stdout, "INITING COPY!!\n");
+}
+
+inline PrepRegion::PrepRegion(const PrepRegion& prep_region) : name(prep_region.name), prep(prep_region.prep), region(R3Box(prep_region.region))
+{}
+
+// Inline Functions
+inline const char* PrepRegion::
+Name(void)
+{
+    return name;
+}
+
+inline int PrepRegion::
+Preposition(void)
+{
+    return prep;
+}
+
+inline R3Box PrepRegion::
+Region(void)
+{
+    return region;
+}*/
+
+
 void UpdatePrepositions(R3Scene* scene, std::vector<R3SceneNode*> objects,
         PrepMap* prep_map, FrequencyStats freq_stats, Id2CatMap* id2cat);
+std::map<int, PrepRegion> InitPrepositions(R3SceneNode* node);
+void CalcPrepositions(R3SceneNode* pri_obj, R3SceneNode* ref_obj, std::string pri_cat, std::string ref_cat,
+        Id2CatMap* id2cat, std::map<int, PrepRegion> prep_region_map);
 
 #endif
--- gaps/apps/p5danalyze/R2Aux.h
+++ gaps/apps/p5danalyze/R2Aux.h
@@ -9,17 +9,23 @@
 
 struct DrawingValues {
     R2Vector translation;
+    R3Vector translation3D;
     RNAngle theta;
     bool do_fX;
     bool do_fY;
     R2Vector *dist;
+    R3Vector *dist3D;
 };
+
+DrawingValues CreateDrawingValues(R3SceneNode* node, int resolution);
 
 // Moves vertex inside grid
 std::vector<R2Point> MoveInsideGrid(R2Grid* grid, R2Point v0, R2Point v1, R2Point v2);
+std::vector<R3Point> MoveInsideGrid(R3Grid* grid, R3Point v0, R3Point v1, R3Point v2);
 
 // Verifies whether vertices are outside of the grid
 bool IsOutsideGrid(R2Grid* grid, R2Point v0, R2Point v1, R2Point v2);
+bool IsOutsideGrid(R3Grid* grid, R3Point v0, R3Point v1, R3Point v2);
 
 // Prepares specific transform required by Heatmaps
 R2Grid* DrawObject(R3SceneNode* obj, R2Grid *grid, DrawingValues values, int pixels_to_meters);
