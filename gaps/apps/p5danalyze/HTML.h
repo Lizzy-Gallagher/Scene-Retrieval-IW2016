bbd2a8ff7bf9a37882f3bb179bd3403d1c11e809
LizzyBradley <erb2@princeton.edu>
2016-06-30 17:59:02 -0400

    First draft of new HTML reporting.

---
+++ gaps/apps/p5danalyze/HTML.h
@@ -0,0 +1,9 @@
+#ifndef HTML
+#define HTML
+
+#include <string>
+
+void CreatePage(std::string pri_cat, std::map<std::string, PrepositionStats> spec_prep_map, FrequencyStats freq_stats, const char* prep_names[]);
+
+
+#endif
