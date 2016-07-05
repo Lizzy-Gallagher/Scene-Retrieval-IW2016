7e14f029cc2fc1986f2661aa9abecc8cdcdc4d5b
LizzyBradley <erb2@princeton.edu>
2016-07-05 19:42:52 -0400

    Continued wall work.

--- gaps/apps/p5danalyze/Drawing.cpp
+++ gaps/apps/p5danalyze/Drawing.cpp
@@ -40,10 +40,10 @@ XformValues CreateXformValues(R3SceneNode* node, int resolution, bool is_room) {
 // Prepares specific transform required by Heatmaps
 R2Affine PrepareWorldToGridXform(R3Point cen3, XformValues values, int pixels_to_meters) {
     R2Vector cen = R2Vector(cen3.X(), cen3.Y());
-    
+
     // Start with the identity matrix
     R2Affine world_to_grid_xform = R2identity_affine;
-    
+
     // Transform the distance form src_obj
     if (values.dist != NULL) {
         values.dist->Rotate(-1.0 * values.theta);
@@ -52,7 +52,7 @@ R2Affine PrepareWorldToGridXform(R3Point cen3, XformValues values, int pixels_to
         world_to_grid_xform.Translate(*(values.dist) * pixels_to_meters);
         world_to_grid_xform.Translate(-1.0 * *(values.dist));
     }
-    
+
     // Center
     world_to_grid_xform.Translate(values.translation);
 
@@ -71,8 +71,8 @@ R3Affine PrepareWorldToGridXform(R3Point cen_pnt, XformValues values)
 {
     R3Vector cen = R3Vector(cen_pnt.X(), cen_pnt.Y(), cen_pnt.Z());
 
-   // Start with the identity matrix
-   R3Affine world_to_grid_xform = R3identity_affine;
+    // Start with the identity matrix
+    R3Affine world_to_grid_xform = R3identity_affine;
 
     // Transform the dist3Dance from src_node
     if (values.dist3D != NULL) {
@@ -82,7 +82,7 @@ R3Affine PrepareWorldToGridXform(R3Point cen_pnt, XformValues values)
         world_to_grid_xform.Translate(*(values.dist3D));
         world_to_grid_xform.Translate(-1.0 * *(values.dist3D));
     }
-    
+
     // Center
     world_to_grid_xform.Translate(values.translation3D);
 
@@ -95,6 +95,50 @@ R3Affine PrepareWorldToGridXform(R3Point cen_pnt, XformValues values)
 
     return world_to_grid_xform;
 }
+
+R2Grid* DrawElement(R3SceneElement* el, R2Grid *grid, XformValues values, int pixels_to_meters, R3Point cen) {
+    R2Grid temp_grid = R2Grid(grid->XResolution(), grid->YResolution());
+
+    R2Affine world_to_grid_xform = PrepareWorldToGridXform(cen, values, pixels_to_meters);
+    temp_grid.SetWorldToGridTransformation(world_to_grid_xform);
+
+    // For all R3Shapes in the R3SceneElements    
+    for (int l = 0; l < el->NShapes(); l++) {
+
+        R3Shape* shape = el->Shape(l);
+        R3TriangleArray* arr = (R3TriangleArray*) shape;
+
+        // For all R3Triangles in the R3TriangleArray
+        for (int t = 0; t < arr->NTriangles(); t++) {
+            R3Triangle *triangle = arr->Triangle(t);
+
+            // Create new points
+            R2Point v0 = R2Point(triangle->V0()->Position().X(), triangle->V0()->Position().Y());
+            R2Point v1 = R2Point(triangle->V1()->Position().X(), triangle->V1()->Position().Y());
+            R2Point v2 = R2Point(triangle->V2()->Position().X(), triangle->V2()->Position().Y());
+
+            if (IsOutsideGrid(&temp_grid, v0, v1, v2)) continue;
+
+            // Move exterior verticies inside the grid
+            std::vector<R2Point> v = MoveInsideGrid(&temp_grid, v0, v1, v2);
+
+            v0 = temp_grid.GridPosition(v[0]);
+            v1 = temp_grid.GridPosition(v[1]);
+            v2 = temp_grid.GridPosition(v[2]);
+
+            temp_grid.RasterizeWorldTriangle(v[0], v[1], v[2], 1);
+        }
+    }
+
+    // Color the shape with a single color
+    temp_grid.Threshold(0, 0, 1);
+    grid->Add(temp_grid);
+    return grid;
+}
+
+
+
+/* For Objects... */
 
 R2Grid* DrawObject(R3SceneNode* obj, R2Grid *grid, XformValues values, int pixels_to_meters)
 {
@@ -149,7 +193,7 @@ R3Grid* DrawObject(R3SceneNode* node, R3Grid *grid, XformValues values)
 
     //XformValues values = CreateXformValues(node, grid->XResolution());
     R3Affine world_to_grid_xform = PrepareWorldToGridXform(node->Centroid(), values);
-    
+
     // For all R3SceneElements in the R3SceneNode
     for (int k = 0; k < node->NElements(); k++) {
         R3SceneElement* el = node->Element(k);
