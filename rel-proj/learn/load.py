bd878d0ed7e754b8ac95baeb6ca94b59bc1c3a0b
LizzyBradley <erb2@princeton.edu>
2016-07-29 17:31:03 -0400

    Add dataset loading infrastructure for scikit-learn. Fixes #38.

---
+++ rel-proj/learn/load.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+arff_buffer = 6
+
+class Dataset(object):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+def csv_file(filename):
+    with open(filename, "r") as f:
+        # count the columns
+        num_cols = len(f.readline().split(","))
+        
+        # reset file
+        f.seek(0)
+        
+        # read dataset
+        dataset = np.genfromtxt(
+            f,
+            dtype=np.int,
+            delimiter=",",
+            usecols=range(1, num_cols))
+
+        X = dataset[:,1:]
+        y = dataset[:,:1].ravel()
+
+        return Dataset(X,y)
+
+def arff_file(filename):
+    with open(filename, "r") as f:
+        # count the columns
+        while True:
+            ln = f.readline()
+            if "@" not in ln and len(ln) > 1:
+                break
+
+        num_cols = len(f.readline().split(","))
+        
+        # reset file
+        f.seek(0)
+        
+        # read dataset
+        dataset = np.genfromtxt(
+            f,
+            dtype=np.int,
+            delimiter=",",
+            skip_header = num_cols + arff_buffer,
+            usecols=range(1, num_cols))
+        
+        X = dataset[:,1:]
+        y = dataset[:,:1].ravel()
+
+        return Dataset(X,y)
+
